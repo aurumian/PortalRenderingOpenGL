b526211a5819b5ffc3ebe1f03641e3721b904f5b
YTitan <tcherezovyuri@mail.ru>
2019-09-10 17:30:23 +0300

    hello window lesson complete

--- OpenGLProject/Main.cpp
+++ OpenGLProject/Main.cpp
@@ -5,3 +5,66 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+/* window resize callback
+ * tell OpenGL that the rendering window size changed
+*/
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+	//initialize glfw
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//create window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//initialze glad
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialie GLAD" << std::endl;
+		return -1;
+	}
+
+	//tell OpenGL the size of the rendering window
+	glViewport(0, 0, 800, 600);
+	//register window resize callback
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	//define vertices to draw
+	float vertices[] = {
+		-0.5f,
+	};
+
+	//render loop
+	while (!glfwWindowShouldClose(window)) {
+		//input
+		processInput(window);
+
+		//rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//checl all the events and swap the buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//clean glfw resources
+	glfwTerminate();
+	return 0;
+}
+
