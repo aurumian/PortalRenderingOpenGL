6af22e0c7548553a995317a7853c75dc6384390a
YTitan <tcherezovyuri@mail.ru>
2019-10-01 21:22:54 +0300

    Blending lesson complete

--- OpenGLProject/Main.cpp
+++ OpenGLProject/Main.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 #include "stb_image.h"
 #include <glad/glad.h>
@@ -68,9 +69,6 @@ void processInput(GLFWwindow* window, Shader* shader = NULL) {
 
 
 int main() {
-	
-	
-
 	//initialize glfw
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -137,6 +135,46 @@ int main() {
 	if (data) {
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(specTex.id);
+	}
+	else {
+		std::cout << "Failed to load texture2" << endl;
+	}
+	// free memory
+	stbi_image_free(data);
+
+	// generate grass texture object
+	Texture grassTex;
+	grassTex.type = "texture_diffuse";
+	glGenTextures(1, &grassTex.id);
+	glBindTexture(GL_TEXTURE_2D, grassTex.id);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	data = stbi_load("D:\\MyFiles\\SomeStuff\\grass.png", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(grassTex.id);
+	}
+	else {
+		std::cout << "Failed to load texture2" << endl;
+	}
+	// free memory
+	stbi_image_free(data);
+
+	// generate grass texture object
+	Texture windowTex;
+	grassTex.type = "texture_diffuse";
+	glGenTextures(1, &windowTex.id);
+	glBindTexture(GL_TEXTURE_2D, windowTex.id);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	data = stbi_load("D:\\MyFiles\\SomeStuff\\window.png", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(windowTex.id);
 	}
 	else {
 		std::cout << "Failed to load texture2" << endl;
@@ -212,9 +250,39 @@ int main() {
 	texs.push_back(specTex);
 	Mesh container = Mesh(verts, inds, texs);
 
+	// create flloor mesh
+	verts.clear();
+	verts.push_back({glm::vec3(1, 0, -1), glm::vec3(0,1,0), glm::vec2(1, 1)});
+	verts.push_back({glm::vec3(1, 0, 1), glm::vec3(0,1,0), glm::vec2(1, 0)});
+	verts.push_back({glm::vec3(-1, 0, 1), glm::vec3(0,1,0), glm::vec2(0, 0)});
+	verts.push_back({glm::vec3(-1, 0, -1), glm::vec3(0,1,0), glm::vec2(0, 1)});
+	inds.clear();
+	inds.push_back(0);
+	inds.push_back(1);
+	inds.push_back(2);
+	inds.push_back(2);
+	inds.push_back(3);
+	inds.push_back(0);
+	Mesh floor = Mesh(verts, inds, texs);
+
+	// create grass mesh
+	verts.clear();
+	verts.push_back({ glm::vec3(0.5f, -0.5f, 0.0f), glm::vec3(0,0,-1), glm::vec2(1, 0) });
+	verts.push_back({ glm::vec3(0.5f, 0.5f, 0.0f), glm::vec3(0,0,-1), glm::vec2(1, 1) });
+	verts.push_back({ glm::vec3(-0.5f, 0.5f, 0.0f), glm::vec3(0,0,-1), glm::vec2(0, 1) });
+	verts.push_back({ glm::vec3(-0.5f, -0.5f, 0.0f), glm::vec3(0,0,-1), glm::vec2(0, 0) });
+	texs.clear();
+	texs.push_back(grassTex);
+	Mesh grass = Mesh(verts, inds, texs);
+
+	// create window mesh
+	texs.clear();
+	texs.push_back(windowTex);
+	Mesh windowMesh = Mesh(verts, inds, texs);
+
 	// positions of all containers
 	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(0.0f,  3.0f,  0.0f),
 		glm::vec3(2.0f,  5.0f, 15.0f),
 		glm::vec3(-1.5f, -2.2f, 2.5f),
 		glm::vec3(-3.8f, -2.0f, 12.3f),
@@ -226,11 +294,35 @@ int main() {
 		glm::vec3(-1.3f,  1.0f, 1.5f)
 	};
 
+	// positions of pieces of grass
+	glm::vec3 grassPositions[] = {
+		glm::vec3(0.2f, 0.0f, 0.1f),
+		glm::vec3(0.25f, 0.0f, 0.19f),
+		glm::vec3(-0.25f, 0.0f, 0.19f),
+		glm::vec3(-0.25f, 0.0f, -0.19f),
+		glm::vec3(-0.5f, 0.0f, -0.34f),
+		glm::vec3(-0.5f, 0.0f, 0.34f),
+		glm::vec3(0.5f, 0.0f, 0.34f),
+		glm::vec3(0.5f, 0.0f, 0.34f)
+	};
+	glm::vec3 windowPositions[] = {
+		glm::vec3(0, 0, 2.0f),
+		glm::vec3(0.5, 0, 1.0f),
+		glm::vec3(0.25f, 0, -1.0f),
+		glm::vec3(-0.25f, 0, 1.5f),
+	};
+
 	//create shaderProgram
 	Shader sp = Shader("D:\\VSProjects\\OpenGLProject\\OpenGLProject\\VertexShader.vs", "D:\\VSProjects\\OpenGLProject\\OpenGLProject\\FragmenShader.fsf");
 
 	// create bulb shader program
 	Shader bulbSP = Shader("D:\\VSProjects\\OpenGLProject\\OpenGLProject\\BulbVertexShader.vs", "D:\\VSProjects\\OpenGLProject\\OpenGLProject\\BulbFragmentShader.fsf");
+
+	// create grass shader program
+	Shader grassShader = Shader("D:\\VSProjects\\OpenGLProject\\OpenGLProject\\VertexShader.vs", "D:\\VSProjects\\OpenGLProject\\OpenGLProject\\GrassFragmentShader.fsf");
+
+	// create window shader program
+	Shader windowShader = Shader("D:\\VSProjects\\OpenGLProject\\OpenGLProject\\VertexShader.vs", "D:\\VSProjects\\OpenGLProject\\OpenGLProject\\WindowFragmentShader.fsf");
 
 	// initialize mouse input
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
@@ -244,20 +336,12 @@ int main() {
 	bulbTransform.rotation.SetEulerAngles(glm::vec3(0.0f, 0.0f, 45.0f));
 	bulbTransform.SetScale(glm::vec3(0.25f, 0.25f, 0.25f));
 
-	{
-		glm::vec3 test = glm::mat3(camera.GetWorldToViewMatrix()) * glm::vec3(0.0f, -1.0f, 0.0f);
-		cout << "viewspace light dir: " << test.x << " " << test.y << " " << test.z << endl;
-	}
-
 	// set initial camera position
 	{
 		Transform t = camera.GetTransform();
 		t.position = glm::vec3(0.0f, 0.0f, -3.0f);
 		camera.SetTransform(t);
 	}
-
-
-	
 
 	Time::Init();
 	//render loop
@@ -270,9 +354,9 @@ int main() {
 		//rendering
 		//open gl render config
 		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-		glEnable(GL_STENCIL_TEST);
-		glStencilMask(0xFF);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
 		sp.use();
@@ -296,16 +380,8 @@ int main() {
 		// set material properties
 		sp.setFloat("material.shiness", 32.0f);
 
-		glStencilFunc(GL_ALWAYS, 1, 0xFF);
-		glStencilOp(GL_KEEP, GL_REPLACE, GL_REPLACE);
-		glStencilMask(0xFF);
+		// draw crates
 		for (int i = 0; i < 10; i++) {
-			if (i % 4 == 0) {
-				glStencilMask(0xFF);
-			}
-			else {
-				glStencilMask(0x00);
-			}
 			Transform transform;
 			transform.rotation.SetEulerAngles(glm::vec3(i* 20.0f, i*45.0f, 0.0f));
 			transform.position = cubePositions[i];
@@ -317,8 +393,34 @@ int main() {
 			sp.SetMat3("normalMatrix", normalMatrix);
 			container.Draw(sp);
 		}
-		glStencilMask(0x00);
-		
+
+		// draw floor
+		Transform transform;
+		transform.position = glm::vec3(0, -0.5f, 3);
+		transform.SetScale(glm::vec3(10));
+		glm::mat4 objectToWorld = transform.GetTransformMatrix();
+		sp.setMat4("objectToWorld", objectToWorld);
+		glm::mat3 normalMatrix = glm::transpose(glm::inverse(glm::mat3(worldToView * objectToWorld)));
+		sp.SetMat3("normalMatrix", normalMatrix);
+		floor.Draw(sp);
+
+		// draw grass
+		grassShader.use();
+		grassShader.setMat4("worldToView", worldToView);
+		grassShader.setMat4("projection", projection);
+		for (int i = 0; i < 8; i++) {
+			Transform transform;
+			transform.rotation.SetEulerAngles(glm::vec3(0.0f , i*17.5f, 0.0f));
+			transform.position = grassPositions[i];
+
+			glm::mat4 objectToWorld = transform.GetTransformMatrix();
+			grassShader.setMat4("objectToWorld", objectToWorld);
+			glm::mat3 normalMatrix = glm::transpose(glm::inverse(glm::mat3(worldToView * objectToWorld)));
+			grassShader.SetMat3("normalMatrix", normalMatrix);
+			grass.Draw(grassShader);
+		}
+
+		// draw bulb
 		bulbSP.use();
 		// set uniform values
 		bulbSP.setMat4("worldToView", worldToView);
@@ -327,23 +429,28 @@ int main() {
 		bulbSP.setMat4("objectToWorld", bulbTransform.GetTransformMatrix());
 		container.Draw(bulbSP);
 
-		glBindVertexArray(0);
-
-		// draw outline for a few objects
-		glDisable(GL_DEPTH_TEST);
-		glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
-		bulbSP.setVec3("lightColor", 1.0f, 1.0f, 0.0f);
-		for (int i = 0; i < 10; i+=4) {
+		// draw windows
+		windowShader.use();
+		windowShader.setMat4("worldToView", worldToView);
+		windowShader.setMat4("projection", projection);
+		// sort the positions to draw the farthest first
+		sort(windowPositions, windowPositions + 4,
+			[](const glm::vec3& x, const glm::vec3& y) {
+			float xDist = glm::length(camera.GetTransform().position - x);
+			float yDist = glm::length(camera.GetTransform().position - y);
+			return xDist > yDist;
+			}
+		);
+		for (int i = 0; i < 4; i++) {
 			Transform transform;
-			transform.rotation.SetEulerAngles(glm::vec3(i* 20.0f, i*45.0f, 0.0f));
-			transform.position = cubePositions[i];
-			transform.SetScale(glm::vec3(1.1f, 1.1f, 1.1f));
+			//transform.rotation.SetEulerAngles(glm::vec3(0.0f, i*17.5f, 0.0f));
+			transform.position = windowPositions[i];
 
 			glm::mat4 objectToWorld = transform.GetTransformMatrix();
-			bulbSP.setMat4("objectToWorld", objectToWorld);
+			windowShader.setMat4("objectToWorld", objectToWorld);
 			glm::mat3 normalMatrix = glm::transpose(glm::inverse(glm::mat3(worldToView * objectToWorld)));
-			bulbSP.SetMat3("normalMatrix", normalMatrix);
-			container.Draw(bulbSP);
+			windowShader.SetMat3("normalMatrix", normalMatrix);
+			windowMesh.Draw(windowShader);
 		}
 		
 		//check all the events and swap the buffers
